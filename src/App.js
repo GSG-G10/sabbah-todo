47a0e1c2973c5680f72ba01e437b6fe561e07898
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-09-28 01:04:35 +0300

    create deleting function and modal for confirm delete & prettier format

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import './App.css';
-import 'antd/dist/antd.css';
-import AddInput from './Components/Input';
-import AddForm from './Components/Form';
-import  Card  from './Components/Card/Card';
-
-import {Button } from 'antd'
+import AddForm from "./Components/Form";
+import Card from "./Components/Card/Card";
+import { Button } from "antd";
+import { useState } from "react";
+import "./App.css";
+import "antd/dist/antd.css";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
   return (
     <div className="App">
       <h1>To Do List</h1>
-      <AddForm />
-      <Card />
-      <Button type="primary" danger > Clear </Button>
+      <AddForm tasks={tasks} setTasks={setTasks} />
+      <Card tasks={tasks} setTasks={setTasks} />
+      <br />
+      <Button type="primary" danger onClick={() => setTasks([])}>
+        {" "}
+        Clear All{" "}
+      </Button>
     </div>
   );
 }
